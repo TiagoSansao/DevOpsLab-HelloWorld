ad4dbaa05f13020d719566b0164190e2d24a3b7f
tiagosansao <tiagossansao@gmail.com>
2021-10-30 14:26:32 -0300

    Bugfix

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pineline Sansao")
+        self.assertRegex(result.data.decode(), "Pipeline Sansao")
 
 
 if __name__ == "__main__":
